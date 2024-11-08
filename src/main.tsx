69fd253c54cd340164d41a462f65c33536a4cc60
Ashley Stockton <styxx1204@gmail.com>
2024-11-08 09:05:02 -0600

    turned auto save on

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Auth0Provider
+    domain=''
+    clientId='ueoObpRFKbm6LmspWUlIoyko5zmP7jEF'
+    authorizationParams={{ redirect_uri: window.location.origin }}
+  >
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </Auth0Provider>
 )
